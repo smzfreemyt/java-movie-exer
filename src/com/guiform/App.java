4e90ec2607c2a2f131eeaa88160ea0aa2b57f3f8
Samuel Amador <samuel.amador@freemight.com>
2021-06-05 10:45:14 +0800

    Add

--- src/com/guiform/App.java
+++ src/com/guiform/App.java
@@ -8,6 +8,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Vector;
 
 public class App  extends JFrame {
@@ -36,20 +38,14 @@ public class App  extends JFrame {
     }
 
     public void populate() {
-//        DefaultTableModel model = new DefaultTableModel();
-//        String[] columns = {"id", "Title", "Details"};
-//        ArrayList<Object> all = this.movie.getAll();
-//        model.addColumn(columns);
-////        for (int rec=0; rec < all.size(); rec++) {
-//        model.addRow(new Object[] {"Sam", "Amador", "test"});
-////        }
-//        tableMovieList.setModel(model);
-
-        DefaultTableModel model = new DefaultTableModel(new Object[]{"Name","Symbol","Atomic Number"},0);
-        Object[] rows = {"sam", "amador", "test"};
-        Object[] row2 = {"second", "amador", "test"};
-        model.addRow(rows);
-        model.addRow(row2);
+        DefaultTableModel model = new DefaultTableModel(new Object[]{"ID","Movie Title","Details"},0);
+        model.addRow(new Object[] {"wow", "this", "test"});
+        ArrayList<Object> obj = this.movie.getAll();
+        int total = obj.size();
+        for (int x=0; x < total; x ++) {
+            Object data = obj.get(x);
+            model.addRow(new Object[]{data});
+        }
         tableMovieList.setModel(model);
     }
 
